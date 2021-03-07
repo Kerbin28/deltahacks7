36e8bda5323cca174b31aadace5e31da7a109c9d
Kerbin28 <kevin01@hotmail.ca>
2021-03-06 19:11:01 -0500

    Updated Java Code

---
+++ DesktopApplication/src/BluetoothCommunicator.java
@@ -0,0 +1,51 @@
+
+import com.fazecast.jSerialComm.SerialPort;
+import java.util.*;
+import java.io.*;
+
+public class BluetoothCommunicator {
+    private static final SerialPort ports[] = SerialPort.getCommPorts();
+    private SerialPort port;
+
+    public BluetoothCommunicator(int port) {
+        this.port = ports[port];
+
+    }
+
+    public BluetoothCommunicator() {
+        int j=0;
+        for(SerialPort i:ports){
+            System.out.println(j++ + ". "+i.getSystemPortName());
+        }
+        this.port = ports[1];
+    }
+
+    public void setPort(int port) {
+        this.port = ports[port];
+    }
+
+    public String readLine() {
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING,0,0);
+        Scanner input = new Scanner(port.getInputStream());
+        String inputText="";
+        port.openPort();
+        while(input.hasNextLine()) {
+
+            inputText= input.nextLine();
+            input.close();
+            port.closePort();
+            return inputText;
+        }
+        input.close();
+        port.closePort();
+        return null;
+    }
+    /*
+    public void sendStringToComm(String command) throws Exception {
+        port.openPort();
+        port.writeBytes(command.getBytes(), command.length());
+        port.closePort();
+    }
+
+     */
+}
--- DesktopApplication/src/Run.java
+++ DesktopApplication/src/Run.java
@@ -1,5 +1,7 @@
 public class Run {
     public static void main(String[] args){
         MainWindow main = new MainWindow();
+        //BluetoothCommunicator bc=new BluetoothCommunicator();
+        //System.out.println(bc.readLine());
     }
 }
