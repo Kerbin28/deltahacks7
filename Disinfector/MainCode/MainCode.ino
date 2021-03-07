44dd5c058ff53772b84155baf2d7d66c584be33f
Keeana Mihalin <53986692+kmihalin@users.noreply.github.com>
2021-03-06 22:16:24 -0500

    Update MainCode.ino

--- Disinfector/MainCode/MainCode.ino
+++ Disinfector/MainCode/MainCode.ino
@@ -4,34 +4,30 @@
 #include <Wire.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-const int sensorPin = A0;    // select the analog input pin
-const int ledPin = 3;       // select the pin for the output
-const int ledpurple = 5; //refill indicator led pin
-const int button = 6; 
+
+int sensorPin = A0;    // select the analog input pin
+int ledPin = 3;       // select the pin for the output
+int sensorValue;   // variable to store the value coming from the sensor
+int ledpurple = 5;
+int button = 6;
 const int MAXCOUNT = 50;
-const int Motorpin = 9;
-//const int MAXposition = 120;
-//const int MINposition = 30;
+int Motorpin = 2;
+int spraypin = 7;
 const int ECHO_PIN = 11;
 const int TRIGGER_PIN = 12;
 const int MAX_PEOPLE = 10;
 
-int sensorValue;   // variable to store the value coming from the sensor
-bool isOpen = false;
-int count = 0;
-bool refill = 0;
-int PersonCount = 0;
-
-//Servo motor;
 NewPing sonar(TRIGGER_PIN,ECHO_PIN,200);
-
+ 
 void setup () {
-  pinMode(ledPin, OUTPUT); //setup output
-  pinMode(button, INPUT); 
-  pinMode(ledpurple, OUTPUT);
+  pinMode (ledPin, OUTPUT); //setup output
+  pinMode (ledpurple, OUTPUT); //setup output
+  pinMode (button, INPUT); //setup output
   pinMode(Motorpin, OUTPUT);
-  //motor.attach(Motorpin);
+  pinMode(spraypin, OUTPUT);
+
   Serial.begin (9600); //setup communication rate
+  
   lcd.begin();
   lcd.backlight();
   lcd.clear();
@@ -45,7 +41,15 @@ void setup () {
   SendState(1);
 }
 
-void loop () {
+int buttonState = 0;
+bool isOpen = false;
+int count = 0;
+bool refill = 0;
+int PersonCount = 0;
+
+ void loop () {
+  int distance = sonar.ping_cm();
+  Serial.println(distance);
   sensorValue = analogRead (sensorPin);  //setup variable that stores sensor's return values
   delay(100);
   if (!isOpen&&sensorValue>100) //Detect if the door is opened
@@ -54,9 +58,9 @@ void loop () {
   }
   else if(isOpen&&sensorValue<50) //Detect if the door got closed
   {
-    delay(500);
+    delay(1000);
     int initial_dist = sonar.ping_cm();   //find the initial distance after the door is closed
-    delay(200);
+    delay(1000);
     int final_dist = sonar.ping_cm();     //find the final distance after the door is closed
     if(refill == 0)
     {
@@ -109,9 +113,12 @@ void loop () {
 void Disinfect()
 {
     digitalWrite(ledPin, HIGH); 
-    digitalWrite(Motorpin,HIGH); 
-    delay(500);
-    digitalWrite(Motorpin,LOW);
+    digitalWrite(spraypin,HIGH); 
+    delay(2000);
+    digitalWrite(Motorpin,HIGH);
+    digitalWrite (spraypin, LOW);
+    delay(2000);
+    digitalWrite (Motorpin, LOW);
     digitalWrite (ledPin, LOW);
 }
 
